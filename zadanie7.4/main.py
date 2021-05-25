1cbe14a29713b9053e71f7f4d84b085d5fbd8308
Maciej Ledzion <maciejledzion96@gmail.com>
2021-05-25 18:09:34 +0200

    nowe repo

---
+++ zadanie7.4/main.py
@@ -0,0 +1,41 @@
+from faker import Faker
+
+fake = Faker()
+class BaseContact:
+    def __init__(self, imie, nazwisko, telefon, email):
+        self.imie=imie
+        self.nazwisko=nazwisko
+        self.telefon=telefon
+        self.email=email
+
+    def contact(self):
+        return f'Wybieram numer {self.telefon} i dzwonię do {self.imie}, {self.nazwisko}'
+
+    @property
+    def name_length(self):
+        return len(self.imie) + len(self.nazwisko) + 1
+
+class BusinessContact(BaseContact):
+    def __init__(self,stanowisko,nazwa_firmy,telefon_s,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.stanowisko=stanowisko
+        self.nazwa_firmy=nazwa_firmy
+        self.telefon_s=telefon_s
+
+    def contact(self):
+        return f'Wybieram numer {self.telefon_s} i dzwonię do {self.imie}, {self.nazwisko}'
+
+    @property
+    def name_length(self):
+        return len(self.imie) + len(self.nazwisko) + 1
+
+person = BusinessContact(imie=fake.name(),nazwisko=fake.name(),)
+
+    def create_contacts(contact_type,x):
+        if contact_type = 'BusinessContact'
+
+        elif contact_type = ''
+
+
+
+
